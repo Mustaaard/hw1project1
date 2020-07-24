4a832648e3ba7b75bcc9270b9a1fc5367c541547
FelixWangXu <fefebigboy@gmail.com>
2020-07-24 23:00:02 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+def main():       
+  print("Hello!")    
+  print("I can add two number for you")       
+  num1 = eval(input("Enter one whole numbers(Ex. 52):"))    
+  num2 = eval(input("Enter another whole numbers(Ex. 41):"))    
+  percentage = (num1 / num2 ) * 100
+  print("Percentage = ", percentage)      
+  sum = num1 + num2     
+  print("The sum of the two number is", sum)
+main()

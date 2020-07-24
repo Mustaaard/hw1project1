90986969ae686f475ffaf5de8cdf9a88c0b3373a
FelixWangXu <fefebigboy@gmail.com>
2020-07-24 23:09:53 +0000

    instead of including percentage, includes difference of two numbers instead

--- main.py
+++ main.py
@@ -2,9 +2,11 @@ def main():
   print("Hello!")    
   print("I can add two number for you")       
   num1 = eval(input("Enter one whole numbers(Ex. 52):"))    
-  num2 = eval(input("Enter another whole numbers(Ex. 41):"))    
-  percentage = (num1 / num2 ) * 100
-  print("Percentage = ", percentage)      
+  num2 = eval(input("Enter another whole numbers(Ex. 41):"))        
   sum = num1 + num2     
-  print("The sum of the two number is", sum)
+  print("The sum of the two numbers is", sum)
+  if num1 > num2:
+    print("The difference between the two numbers is", num1-num2)
+  else:
+    print("The difference between the two numbers is", num2-num1)
 main()
